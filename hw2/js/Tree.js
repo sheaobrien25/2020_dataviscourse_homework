dda3d196e6e5ee94cad077dfaf51bd58ec8208ff
u6012545 <u6012545@UH003567M.local>
2020-09-16 16:46:56 -0600

    Final Changes to HW2

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -11,19 +11,22 @@ class Tree {
             return new Node(input.name, input.parent)
         })
         console.log(this.animallist);
+
+        for (let animalNode of this.animallist) {
+            let parentNode = this.animallist.find(node => node.name === animalNode.parentName)
+            if (parentNode) {
+                animalNode.parentNode = parentNode;
+                parentNode.addChild(animalNode);
+            }
+        }
+        console.log(this.animallist);
+        // console.log('the parent of ' + animalNode.name + ' is ' + parentNode.name);//.addChild(animalNode);
     }
 
     /**
      * Function that builds a tree from a list of nodes with parent refs
      */
     buildTree() {
-        for (let animalNode of this.animallist) {
-            let parentNode = this.animallist.find(node => node.name === animalNode.parentName)
-            if (parentNode) {
-                parentNode.addChild(animalNode);
-            }
-        }
-        console.log(this.animalNode);
         // note: in this function you will assign positions and levels by making calls to assignPosition() and assignLevel()
         // use recursion - tree traversal, depth first search and breadth first search
 
@@ -63,9 +66,7 @@ class Tree {
          * Recursive function to traverse the tree and find the child with the largest position
          */
         function findLastPosition(node) {
-
             let lastChild = node.children[node.children.length - 1]
-
             if (lastChild === undefined) {
                 return node.position
             } else {
@@ -94,30 +95,34 @@ class Tree {
         let body = d3.select("body")
 
         let svg = body.append("svg")
-            .attr("height", 1200)
+            .attr("height", 1200) //defining the "canvas" in which to draw the diagram
             .attr("width", 1200)
 
-        let children = this.ist.filter(node => node.parentNode)
+        let children = this.animallist.filter(node => node.parentNode) //start with children because there will always be one single parent node to draw to 
         svg.selectAll("line")
-            .data(children)
+            .data(children) //where to find the data
             .enter()
             .append("line")
-            .attr("x1", childNode => (childNode.parentNode.level * 155) + 155)
-            .attr("y1", childNode => (childNode.parentNode.position * 155) + 155)
-            .attr("x2", childNode => (childNode.level * 155) + 155)
-            .attr("y2", childNode => (childNode.position * 155) + 155)
+            .attr("x1", childNode => (childNode.parentNode.level * 145) + 145)
+            .attr("y1", childNode => (childNode.parentNode.position * 145) + 145)
+            .attr("x2", childNode => (childNode.level * 145) + 145)
+            .attr("y2", childNode => (childNode.position * 145) + 145)
 
-        var jsonCircles = [
+        let selection = svg.selectAll("g")
+            .data(this.animallist)
+            .enter()
+            .append("g")
+            .attr("class", "nodeGroup")
+            .attr("transform", d => `translate (${(d.level *145) +145}, ${(d.position *145) + 145})`)
+            //translate uses the x and y coordinates and moves them along as "g", d = data (animallist)
 
-            var svgContainer = d3.select("body").append("svg")
-                .attr("width", 300)
-                .attr("height", 300);
-        ]
+        selection.append("circle")
+            .attr("r", 35)
 
+        selection.append("text")
+            .attr("class", "label")
+            .html(d => d.name)
 
-        .html(d => d.name)
-
-        console.log(children)
     }
 
 }
